63f0f22f26e68bf82911dc44a09c56ff10473a84
ivainqueur <ishimvainqueur@gmail.com>
2023-12-18 11:27:04 +0200

    [fix] e2e mockbean isuue

--- src/test/java/com/example/vainqueurishimweterm1a/MathOperationsEndToEndTest.java
+++ src/test/java/com/example/vainqueurishimweterm1a/MathOperationsEndToEndTest.java
@@ -2,17 +2,21 @@ package com.example.vainqueurishimweterm1a;
 
 import com.example.vainqueurishimweterm1a.controllers.MathController;
 import com.example.vainqueurishimweterm1a.dtos.DoMathRequestDto;
+import com.example.vainqueurishimweterm1a.serviceImpls.MathOperatorImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(MathController.class)
@@ -22,14 +26,20 @@ public class MathOperationsEndToEndTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    MathOperatorImpl moi;
+
     @Test
     public void testMathController() throws Exception {
         DoMathRequestDto dto = new DoMathRequestDto(14, 4, "+");
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .post("/api/v1/do_math")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(dto));
+
         mockMvc.perform(request)
-                .andExpect(status().isCreated());
+                .andExpect(status().isOk());
     }
 
 }
