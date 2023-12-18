c4b08749cba9bf249bd78eacb6fabd2a009a9653
ivainqueur <ishimvainqueur@gmail.com>
2023-12-18 09:13:37 +0200

    [init]

---
+++ src/main/java/com/example/vainqueurishimweterm1a/VainqueurishimweTerm1AApplication.java
@@ -0,0 +1,13 @@
+package com.example.vainqueurishimweterm1a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VainqueurishimweTerm1AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VainqueurishimweTerm1AApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vainqueurishimweterm1a/VainqueurishimweTerm1AApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vainqueurishimweterm1a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VainqueurishimweTerm1AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
